a48388e03e877fb1216784fa2e292011833da5e5
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-24 16:10:53 -0300

    feita aula 09

--- AULAS/aula08/db.js
+++ AULAS/aula08/db.js
@@ -1,15 +1,15 @@
 
-const conectar = async ()=>{
-    if(global.conexao && global.conexao.state != "disconected"){
-        return global.conexao
-    }
+// const conectar = async ()=>{
+//     if(global.conexao && global.conexao.state != "disconected"){
+//         return global.conexao
+//     }
 
-    const mysql = require('mysql2/promise')
-    const con = mysql.createConnection('mysql://root:01167671216@localhost:3306/cfbcursos')
-    console.log("conectado ao banco")
-    global.conexao = con
-    return con
-}
+//     const mysql = require('mysql2/promise')
+//     const con = mysql.createConnection('mysql://root:01167671216@localhost:3306/cfbcursos')
+//     console.log("conectado ao banco")
+//     global.conexao = con
+//     return con
+// }
 
 const todosClientes = async ()=>{
     const con = await conectar()
@@ -17,4 +17,17 @@ const todosClientes = async ()=>{
     return await linhas
 }
 
-module.exports = {todosClientes}
+const insereClientes = async (cliente)=>{
+    const con = await conectar()
+    const sql = 'insert into cliente values (?,?,?,?,?)'
+    const valores = [
+        cliente.id,
+        cliente.nome,
+        cliente.cpf,
+        cliente.nascimento,
+        cliente.tipo
+    ]
+    await con.query(sql,valores)
+}
+
+module.exports = {todosClientes,insereClientes}
--- AULAS/aula08/index.js
+++ AULAS/aula08/index.js
@@ -1,5 +1,18 @@
 const dados = async ()=>{
     const db = require('./db')
+
+    console.log('inserir novo cliente')
+    
+    await db.insereClientes(
+        {
+            id:'11', 
+            nome:'romulo', 
+            cpf:'75757575757', 
+            nascimento:'2005-05-19',
+            tipo:'3'
+        }
+    )
+
     console.log('obter todos os clientes')
     const clientes = await db.todosClientes()
     console.log(clientes)
