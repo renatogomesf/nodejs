aa74270568c811f7fb91caa783b89f6f274b6ce5
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-26 18:54:24 -0300

    feita aula 03

--- AULAS/edson maia/API-REST/src/app.js
+++ AULAS/edson maia/API-REST/src/app.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { response } from 'express'
 
-// criar rota padrão
+const app = express()
+
+// mock
+const selecoes = [
+    {id: 1, selecao: 'Brasil', grupo: 'G'},
+    {id: 2, selecao: 'Suíça', grupo: 'G'},
+    {id: 3, selecao: 'Sérvia', grupo: 'G'},
+    {id: 4, selecao: 'Camarões', grupo: 'G'},
+]
+
+
+// criar rota padrão ou raiz
 app.get('/',(request,response)=>{
+    // ".send()": envia uma resposta pra acessar o end point.
     response.send('curso de node.js!')
 })
 
-app.listen(port, ()=>{
-    console.log(`Servidor rodando na endereço http://localhost:${port}`)
+app.get('/selecoes',(request,response)=>{
+    response.status(200).send(selecoes)
 })
+
+export default app
