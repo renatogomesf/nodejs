07abeda9fe38301ecf479172fabf5975a492db8a
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-29 16:21:44 -0300

    feita aula 05

--- AULAS/edson maia/API-REST/src/app.js
+++ AULAS/edson maia/API-REST/src/app.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
@@ -9,9 +9,23 @@ app.use(express.json())
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'}
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'}
 ]
+
+// retornar o objeto por id.
+const buscarSelecaoPorId = (id) => {
+    return selecoes.filter(selecao=>
+        selecao.id == id
+    )
+}
+
+// pegar posição ou index do elemento no array por id.
+const buscaIndexSelecao = (id) => {
+    return selecoes.findIndex(selecao =>
+        selecao.id == id
+    )
+}
 
 
 // criar rota padrão ou raiz
@@ -20,14 +34,34 @@ app.get('/',(request,response)=>{
     response.send('curso de node.js!')
 })
 
+
 app.get('/selecoes',(request,response)=>{
     response.status(200).send(selecoes)
 })
+
+
+// ":'nome do parâmetro'": criando parâmetros
+app.get('/selecoes/:id',(request,response)=>{
+    // pega o parâmetro da requisição
+    // let index = request.params.id
+    response.json(buscarSelecaoPorId(request.params.id))
+})
+
 
 app.post('/selecoes',(request,response)=>{
     // uma requisição tem basicamente duas partes, o HEADER (cabeçalho que tem meta dados e dados importantes a certa da origem dela, tipo e etc.) e o BODY (corpo da requisição/corpo da mensagem... conteúdo de fato.). neste caso estamos pegando o body. através do ".body".
     selecoes.push(request.body)
     response.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+
+app.delete('/selecoes/:id',(request,response)=>{
+    let index = buscaIndexSelecao(request.params.id)
+
+    // ".splice('posição','quantidade')": corta o array numa posição desejada e, a partir dali, apaga a quantidade desejada de itens, contando com o da posição desejada. neste caso, estou cortando o array numa posição e apagando 1 item a partir daquela posição... neste caso, apagando o próprio item da posição definida
+    selecoes.splice(index, 1)
+
+    response.send(`Seleção com id: ${request.params.id} excluída com sucesso!`)
 })
 
 export default app
