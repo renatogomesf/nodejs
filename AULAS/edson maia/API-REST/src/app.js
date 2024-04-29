5efd0196bf7bb9fe922416f29a99ee6b75849578
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-29 15:38:37 -0300

    feita aula 04

--- AULAS/edson maia/API-REST/src/app.js
+++ AULAS/edson maia/API-REST/src/app.js
@@ -2,12 +2,15 @@ import express, { response } from 'express'
 
 const app = express()
 
+// indicar para o express ler body com json.
+app.use(express.json())
+
 // mock
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suíça', grupo: 'G'},
     {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Camarões', grupo: 'G'}
 ]
 
 
@@ -19,6 +22,12 @@ app.get('/',(request,response)=>{
 
 app.get('/selecoes',(request,response)=>{
     response.status(200).send(selecoes)
+})
+
+app.post('/selecoes',(request,response)=>{
+    // uma requisição tem basicamente duas partes, o HEADER (cabeçalho que tem meta dados e dados importantes a certa da origem dela, tipo e etc.) e o BODY (corpo da requisição/corpo da mensagem... conteúdo de fato.). neste caso estamos pegando o body. através do ".body".
+    selecoes.push(request.body)
+    response.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 export default app
